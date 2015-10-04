16ecfe1dd0e0fec2cb327b1a08909481099b0941
okunishinishi <okunishitaka.com@gmail.com>
2015-10-05 08:39:13 +0900

    Bugfix

--- test/index_test.js
+++ test/index_test.js
@@ -19,8 +19,10 @@ exports['index'] = function (test) {
     test.ok(!index(123).is('string'));
     test.ok(index('foo').is('string'));
     test.ok(index('foo').isString());
+    test.ok(index.isString('foo'));
     test.ok(index(function(){}).is('function'));
     test.ok(index(function(){}).isFunction());
+    test.ok(index.isFunction(function(){}));
     test.ok(!index('1234').is('function'));
     test.ok(!index(123).is('function'));
     test.done();
