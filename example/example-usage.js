5a80b999f7e0b3f8509aa2d50d67c7ae29227cc7
okunishinishi <okunishitaka.com@gmail.com>
2015-12-10 13:22:36 +0900

    Version incremented to 2.0.2

--- example/example-usage.js
+++ example/example-usage.js
@@ -12,7 +12,7 @@ iftype(123).is('string'); // => false
 iftype(function foo(){}).is('string'); // => false
 iftype(new CustomType()).is('string'); // => false
 iftype(["foo", "bar"]).is('string'); // => false
-iftype("bar").is('string'); // => false
+iftype("bar").is('string'); // => true
 iftype(null).is('string'); // => false
 iftype(undefined).is('string'); // => false
 
@@ -20,7 +20,7 @@ iftype(undefined).is('string'); // => false
 //----------------------------
 // Check if type is 'number'
 //----------------------------
-iftype(123).is('number'); // => false
+iftype(123).is('number'); // => true
 iftype(function foo(){}).is('number'); // => false
 iftype(new CustomType()).is('number'); // => false
 iftype(["foo", "bar"]).is('number'); // => false
@@ -34,8 +34,8 @@ iftype(undefined).is('number'); // => false
 //----------------------------
 iftype(123).is('object'); // => false
 iftype(function foo(){}).is('object'); // => false
-iftype(new CustomType()).is('object'); // => false
-iftype(["foo", "bar"]).is('object'); // => false
+iftype(new CustomType()).is('object'); // => true
+iftype(["foo", "bar"]).is('object'); // => true
 iftype("bar").is('object'); // => false
 iftype(null).is('object'); // => false
 iftype(undefined).is('object'); // => false
@@ -47,7 +47,7 @@ iftype(undefined).is('object'); // => false
 iftype(123).is('array'); // => false
 iftype(function foo(){}).is('array'); // => false
 iftype(new CustomType()).is('array'); // => false
-iftype(["foo", "bar"]).is('array'); // => false
+iftype(["foo", "bar"]).is('array'); // => true
 iftype("bar").is('array'); // => false
 iftype(null).is('array'); // => false
 iftype(undefined).is('array'); // => false
@@ -57,7 +57,7 @@ iftype(undefined).is('array'); // => false
 // Check if type is 'function'
 //----------------------------
 iftype(123).is('function'); // => false
-iftype(function foo(){}).is('function'); // => false
+iftype(function foo(){}).is('function'); // => true
 iftype(new CustomType()).is('function'); // => false
 iftype(["foo", "bar"]).is('function'); // => false
 iftype("bar").is('function'); // => false
@@ -70,7 +70,7 @@ iftype(undefined).is('function'); // => false
 //----------------------------
 iftype(123).is(CustomType); // => false
 iftype(function foo(){}).is(CustomType); // => false
-iftype(new CustomType()).is(CustomType); // => false
+iftype(new CustomType()).is(CustomType); // => true
 iftype(["foo", "bar"]).is(CustomType); // => false
 iftype("bar").is(CustomType); // => false
 iftype(null).is(CustomType); // => false
